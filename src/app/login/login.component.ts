b592657a4d2ddf2130913cc3b5a5dfcffc2a56ee
angeloferrando <angelo.ferrando42@gmail.com>
2018-05-14 11:21:47 +0200

    create attachment restored

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,15 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {DataService} from '../services/dataService/data.service';
+import config from '../../config';
+import {EntityManager, config as breezeConfig, FilterQueryOp, Entity} from 'breeze-client';
+import {BizFilter} from '../services/dataService/bizFilter';
+import {BizStream} from '../services/dataService/bizStream';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [ DataService ]
 })
 export class LoginComponent implements AfterViewInit {
 
@@ -15,11 +21,37 @@ export class LoginComponent implements AfterViewInit {
 
   }
 
-  constructor() {
+  constructor(private dataService: DataService) {
   }
 
   ngAfterViewInit() {
     this.showModal();
+  }
+
+  login(name: string, surname: string, PIN: number) {
+    console.log('login function called with parameters: ' + name + ', ' + surname + ', ' + PIN);
+    try {
+      const context: EntityManager = new EntityManager(config.SAMPLE_SERVER_BASE_URL);
+
+      this.dataService.getEntitiesParamBinary(
+        'LoginGuest',
+        ({nome: name, cognome: surname, PIN: PIN}),
+        context, data => {
+          console.log('Callback started');
+          // get the current default Breeze AJAX adapter
+          const ajaxAdapter: any = breezeConfig.getAdapterInstance('ajax');
+          console.log(data[0]);
+
+          // set fixed headers
+          ajaxAdapter.defaultSettings = {
+            headers: {
+              'Authorization': data[0]
+            }
+          };
+        });
+    } catch (e) {
+      console.log('Exception: ' + e);
+    }
   }
 
 }
